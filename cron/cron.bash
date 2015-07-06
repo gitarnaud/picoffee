a58f1d5502bd2c920d6f3cfbb8e332bfd2d5f796
root <root@raspberrypi.(none)>
2015-07-06 22:57:54 +0200

    Changed time from 100 minutes to 10 minutes.

--- cron/cron.bash
+++ cron/cron.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 bash /root/picoffee/exe/picoffee.bash start
-running_time=`bash /root/picoffee/exe/picoffee.bash time`
+running_time=`bash /root/picoffee/exe/picoffee.bash running-time`
 
 echo $running_time
 
-if [[ "$running_time" -gt 6000 ]]
+if [[ "$running_time" -gt 600 ]]
 then
 	bash /root/picoffee/exe/picoffee.bash stop
 fi
